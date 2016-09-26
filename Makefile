f3b444720b279b3bcff16e737270ca518826ca31
zakk <zakk0610@gmail>
2016-09-26 14:44:20 +0800

    add dgemm_ test

--- Makefile
+++ Makefile
@@ -1,11 +1,18 @@
 SOURCES = sgemm.c dgemm.c
 OBJECTS =$(SOURCES:.c=.o)
 
+.PHONY: clean
+
 %.o: %.c
-	gcc -o $@ $^ -c -O3 -fPIC
+	gcc -o $@ $^ -c -O0 -fPIC -g3
+
+blas.ldflags = -L/home/zakk/anaconda2/lib -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -Wl,-rpath,/home/zakk/anaconda2/lib
 
 all: $(OBJECTS)
 	gcc $(OBJECTS) -shared -o libmrvlblas.so -fpic
+
+test: test.c
+	gcc $^ -o $@ ${blas.ldflags} -g3
 
 clean:
 	rm *.o libmrvlblas.so
---
+++ test.c
@@ -0,0 +1,84 @@
+/*#### Call BLAS Fortran interface
+
+This example shows calling dgemm Fortran interface in C. https://gist.github.com/xianyi/5780018
+
+```
+*/
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/time.h"
+#include "time.h"
+
+extern void dgemm_(char*, char*, int*, int*, int*, double*, double*, int*, double*, int*, double*, double*, int*);
+
+int main(int argc, char* argv[])
+{
+  int i;
+  printf("test!\n");
+  if(argc<4){
+    printf("Input Error\n");
+    return 1;
+  }
+
+  int m = atoi(argv[1]);
+  int n = atoi(argv[2]);
+  int k = atoi(argv[3]);
+  int sizeofa = m * k;
+  int sizeofb = k * n;
+  int sizeofc = m * n;
+  char ta = 'N';
+  char tb = 'N';
+  double alpha = 1;
+  double beta = 1;
+
+  struct timeval start,finish;
+  double duration;
+
+  double* A = (double*)malloc(sizeof(double) * sizeofa);
+  double* B = (double*)malloc(sizeof(double) * sizeofb);
+  double* C = (double*)malloc(sizeof(double) * sizeofc);
+
+  srand((unsigned)time(NULL));
+
+  printf("A=");
+  for (i=0; i<sizeofa; i++) {
+    A[i] = i%3+1;
+    printf("%f,", A[i]);
+  }
+  printf("\nB=");
+  for (i=0; i<sizeofb; i++) {
+    B[i] = i%3+1;
+    printf("%f,", B[i]);
+  }
+  printf("\nC=");
+  for (i=0; i<sizeofc; i++) {
+    C[i] = i%3+1;
+    printf("%f,", C[i]);
+  }
+  printf("\n");
+
+  printf("m=%d,n=%d,k=%d,alpha=%lf,beta=%lf,sizeofc=%d\n",m,n,k,alpha,beta,sizeofc);
+  gettimeofday(&start, NULL);
+  dgemm_(&ta, &tb, &m, &n, &k, &alpha, A, &m, B, &k, &beta, C, &m);
+  gettimeofday(&finish, NULL);
+
+  printf("result=");
+  for (i=0; i<sizeofc; i++) {
+    printf("%f,", C[i]);
+  }
+  printf("\n");
+
+  duration = ((double)(finish.tv_sec-start.tv_sec)*1000000 + (double)(finish.tv_usec-start.tv_usec)) / 1000000;
+  double gflops = 2.0 * m *n*k;
+  gflops = gflops/duration*1.0e-6;
+
+  FILE *fp;
+  fp = fopen("timeDGEMM.txt", "a");
+  fprintf(fp, "%dx%dx%d\t%lf s\t%lf MFLOPS\n", m, n, k, duration, gflops);
+  fclose(fp);
+
+  free(A);
+  free(B);
+  free(C);
+  return 0;
+}
